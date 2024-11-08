facec6c4134fb4cec1794563b9b277eacc17ada6
Syed Faheemuddin <sd.faheemuddin@gmail.com>
2024-11-08 18:11:50 +0530

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# autolisp-outline-extension
-A VSCode extension to identify AutoLISP commands, functions and display them in the OUTLINE.
+
+# AutoLISP Symbols Extension
+
+## Description
+The **AutoLISP OUTLINE Extension** for Visual Studio Code is designed to enhance your productivity when working with AutoLISP files. This extension automatically identifies symbols i.e., commands and functions defined using `defun` in `.lsp` files and displays them in the VSCode OUTLINE view.
+
+This extension is specifically useful for AutoCAD users who work with AutoLISP files and want an organized way to view their defined commands and functions. It complements Autodesk's official AutoLISP extension by adding symbol support without overriding its syntax highlighting.
+
+## Features
+- Automatically detects `defun` symbols and displays them in the Outline view.
+- Supports both command definitions `(defun c:commandName)` and regular function definitions `(defun functionName)`.
+- Lightweight and easy to use, designed to work seamlessly with existing AutoLISP extensions.
+
+## Installation
+To install this extension:
+1. Download and install the extension from the [Visual Studio Code Marketplace](https://marketplace.visualstudio.com/).
+2. Open a `.lsp` file, and you should see the commands and functions listed in the Outline view.
+
+## How to Use
+1. Open any `.lsp` file in Visual Studio Code.
+2. The extension will automatically detect commands and functions and display them in the **OUTLINE** view (located in the left sidebar).
+3. Use the OUTLINE view to quickly navigate through your AutoLISP code.
+
+## GitHub Repository
+You can find the source code and contribute to the project on GitHub:  
+[GitHub Repository](https://github.com/sdfaheemuddin/autolisp-outline-extension)
+
+## Contact
+Feel free to connect with me for any feedback, suggestions, or queries!
+
+- **Name**: syed Faheemuddin
+- **LinkedIn**: [sdfaheemuddin](https://www.linkedin.com/in/sdfaheemuddin/)
+- **Email**: sd.faheemuddin@gmail.com
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Changelog
+### v1.0.0
+- Initial release: Added support for detecting AutoLISP symbols in `.lsp` files.
---
+++ src/extension.js
@@ -0,0 +1,65 @@
+const vscode = require('vscode');
+
+/**
+ * This function activates the extension.
+ * It registers a Document Symbol Provider for AutoLISP files
+ * to detect functions and commands and display them in the Outline view.
+ */
+function activate(context) {
+    // Register a document symbol provider for AutoLISP files
+    vscode.languages.registerDocumentSymbolProvider({ language: 'autolisp' }, {
+        provideDocumentSymbols(document) {
+            const symbols = [];
+
+            // Regular expression to match commands (defun c:commandName)
+            const regexCommand = /\(defun\s+c:(\w+)/g;
+
+            // Regular expression to match functions (defun functionName)
+            const regexFunction = /\(defun\s+(?!c:)(\w+)/g;
+
+            // Iterate through each line of the document to find matches
+            for (let i = 0; i < document.lineCount; i++) {
+                const line = document.lineAt(i).text;
+                let match;
+
+                // Find and add command symbols to the Outline
+                while ((match = regexCommand.exec(line)) !== null) {
+                    const symbol = new vscode.DocumentSymbol(
+                        match[1], // Command name
+                        'Command', // Symbol description
+                        vscode.SymbolKind.Function, // Symbol kind (function)
+                        new vscode.Range(i, match.index, i, line.length),
+                        new vscode.Range(i, match.index, i, line.length)
+                    );
+                    symbols.push(symbol);
+                }
+
+                // Find and add function symbols to the Outline
+                while ((match = regexFunction.exec(line)) !== null) {
+                    const symbol = new vscode.DocumentSymbol(
+                        match[1], // Function name
+                        'Function', // Symbol description
+                        vscode.SymbolKind.Function, // Symbol kind (function)
+                        new vscode.Range(i, match.index, i, line.length),
+                        new vscode.Range(i, match.index, i, line.length)
+                    );
+                    symbols.push(symbol);
+                }
+            }
+
+            // Return the list of symbols found
+            return symbols;
+        }
+    });
+}
+
+/**
+ * This function deactivates the extension.
+ */
+function deactivate() {}
+
+// Export the activate and deactivate functions
+module.exports = {
+    activate,
+    deactivate
+};
